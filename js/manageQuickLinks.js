f56baf429f44546dc2645ce0516a89fd723bcb66
abdelrahmman97 <mabdelrahmman22@gmail.com>
2022-01-11 12:01:50 +0200

    First Commit

---
+++ js/manageQuickLinks.js
@@ -0,0 +1,201 @@
+class QuicklinksLocalStorage {
+    constructor() {
+        // if item by key `Quicklinks` is not defined JSON.parse return null, so I use `or empty array`
+        this.QuicklinksList = JSON.parse(localStorage.getItem('Quicklinks')) || [];
+    }
+
+    // create link
+    create(data) {
+        data.token = this.token;
+        this.QuicklinksList.push(data);
+        localStorage.setItem('Quicklinks', JSON.stringify(this.QuicklinksList));
+    }
+
+    getIndexByToken(token) {
+        for (let i = 0; i < this.QuicklinksList.length; i++) {
+            if (this.QuicklinksList[i].token === token) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    get token() {
+        return Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
+    }
+
+    update(data) {
+        let index = this.getIndexByToken(data.token);
+        if (index !== -1) {
+            this.QuicklinksList[index] = data;
+            localStorage.setItem('Quicklinks', JSON.stringify(this.QuicklinksList));
+        }
+    }
+
+    delete(data) {
+        let index = this.getIndexByToken(data.token);
+        if (index !== -1) {
+            this.QuicklinksList.splice(index, 1);
+            localStorage.setItem('Quicklinks', JSON.stringify(this.QuicklinksList));
+        }
+    }
+
+}
+
+
+
+
+const qlStorage = new QuicklinksLocalStorage();
+
+const newLink = qlStorage.QuicklinksList;
+
+const container = document.querySelector('#qlinks');
+const template = document.querySelector('#links_template');
+
+const createLinkForm = document.querySelector('#addNewLink_form');
+const createLinkTitle = document.querySelector('#linkTittle');
+const createLinkAddress = document.querySelector('#linkAddress');
+const createLinkButton = document.querySelector('#addLinkBtn');
+
+// const para = document.createElement("p");
+// para.innerText = "Link list is empty!";
+// para.setAttribute("id", "EmptyListText");
+// para.setAttribute("class", "alert-heading text-capitalize text-center text-dark-lm text-light-dm");
+
+function onCreateLink({ data }) {
+
+    // var elementPara = document.getElementById("EmptyListText");
+    // if (elementPara != null) {
+    //     elementPara.parentNode.removeChild(elementPara);
+    // }
+
+    const clone = template.content.cloneNode(true);
+
+    const link = clone.querySelector('.qlink__link');
+    const image = clone.querySelector('.qlink__link img');
+    const title = clone.querySelector('.qlink__link .link_title');
+    const forSVG = clone.querySelector('#forSVG');
+    const edit_btn = clone.querySelector('#edit_btn');
+    const delete_btn = clone.querySelector('#delete_btn');
+
+    var id = halfmoon.makeId(5);
+    link.setAttribute("id", id);
+    link.setAttribute("href", data.link);
+    if (data.thereIsImage) {
+        forSVG.remove();
+        image.setAttribute("src", data.image);
+    }
+    else if (checkInternet()) {
+        forSVG.remove();
+        image.setAttribute("src", "https://t1.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=" + data.link + "&size=64");
+    }
+    else {
+        image.remove();
+        forSVG.innerHTML = data.image
+    }
+    // alert(id);
+
+    title.innerHTML = data.title;
+
+    // toggleTaskStatusClass({
+    //     checked: data.checked,
+    //     task,
+    //     title
+    // });
+
+    // checkbox.addEventListener('input', () => {
+    //     data.checked = checkbox.checked;
+    //     toggleTaskStatusClass({
+    //         checked: data.checked,
+    //         task,
+    //         title
+    //     });
+    //     storage.update(data);
+    // });
+
+    // title.addEventListener('input', () => {
+    //     data.value = title.innerHTML;
+    //     storage.update(data);
+    // });
+
+    delete_btn.addEventListener('click', (e) => {
+        e.preventDefault();
+        qlStorage.delete(data);
+        link.remove();
+        (link == null || link == "") ? container.appendChild(para) : "";
+        var arrayFromStroage = JSON.parse(localStorage.getItem("Quicklinks"));
+        var arrayLength = arrayFromStroage.length;
+        if (arrayLength >= 18) {
+            $("#addNewLink_form").addClass("hidden");
+            return false;
+        }
+        else {
+            $("#addNewLink_form").removeClass("hidden");
+        }
+    });
+
+    container.appendChild(clone);
+
+    var arrayFromStroage = JSON.parse(localStorage.getItem("Quicklinks"));
+    var arrayLength = arrayFromStroage.length;
+    if (arrayLength >= 18) {
+        $("#addNewLink_form").addClass("hidden");
+        return false;
+    }
+    else {
+        $("#addNewLink_form").removeClass("hidden");
+    }
+}
+
+window.addEventListener('online', () =>
+    newLink.forEach((data) => {
+        onCreateLink({
+            data
+        });
+    })
+);
+
+newLink.forEach((data) => {
+    onCreateLink({
+        data
+    });
+});
+
+createLinkForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    var arrayFromStroage = JSON.parse(localStorage.getItem("Quicklinks"));
+    var arrayLength = arrayFromStroage.length;
+    if (arrayLength >= 18) {
+        $("#addNewLink_form").addClass("hidden");
+        return false;
+    }
+    else {
+        $("#addNewLink_form").removeClass("hidden");
+    }
+    const title = createLinkTitle.value;
+    const link = createLinkAddress.value;
+    let image = "";
+    if (checkInternet()) {
+        image = "https://t1.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=" + createLinkAddress.value + "&size=64"
+    } else {
+        image = '<svg class="" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3.055 11H5a2 2 0 012 2v1a2 2 0 002 2 2 2 0 012 2v2.945M8 3.935V5.5A2.5 2.5 0 0010.5 8h.5a2 2 0 012 2 2 2 0 104 0 2 2 0 012-2h1.064M15 20.488V18a2 2 0 012-2h3.064M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>';
+    }
+    console.log(image);
+    if (title && link && image) {
+        const data = {
+            title,
+            link,
+            image,
+            thereIsImage: checkInternet()
+        };
+        qlStorage.create(data);
+        onCreateLink({
+            data
+        });
+        createLinkForm.reset();
+    }
+});
+
+function checkInternet() {
+    return window.navigator.onLine
+}
---
+++ js/time.js
@@ -0,0 +1,115 @@
+const userLang = 'ar';
+const Userformat = true;
+
+window.onload = displayClock();
+
+function displayClock() {
+    var lang = userLang;
+    var format_ampm = Userformat;
+
+    const monthNames = [
+        { ar: "يناير", eng: "Jan" },
+        { ar: "فبراير", eng: "Feb" },
+        { ar: "مارس", eng: "Mar" },
+        { ar: "أبريل", eng: "Apr" },
+        { ar: "مايو", eng: "May" },
+        { ar: "يونيو", eng: "Jun" },
+        { ar: "يوليو", eng: "Jul" },
+        { ar: "أغسطس", eng: "Aug" },
+        { ar: "سبتمبر", eng: "Sep" },
+        { ar: "أكتوبر", eng: "Oct" },
+        { ar: "موفمبر", eng: "Nov" },
+        { ar: "ديسمبر", eng: "Dec" }
+        // 'Jan',
+        // 'Feb',
+        // 'Mar',
+        // 'Apr',
+        // 'May',
+        // 'Jun',
+        // 'Jul',
+        // 'Aug',
+        // 'Sep',
+        // 'Oct',
+        // 'Nov',
+        // 'Dec',
+    ];
+
+    const daysList = [
+        { ar: "الأحد", eng: "Su" },
+        { ar: "الاثنين", eng: "Mo" },
+        { ar: "الثلاثاء", eng: "Tu" },
+        { ar: "الأربعاء", eng: "We" },
+        { ar: "الخميس", eng: "Th" },
+        { ar: "الجمعة", eng: "Fr" },
+        { ar: "السبت", eng: "Sa" }
+        // 'Su',
+        // 'Mo',
+        // 'Tu',
+        // 'We',
+        // 'Th',
+        // 'Fr',
+        // 'Sa',
+    ];
+
+    const am = { ar: " ص ", eng: " am" };
+    const pm = { ar: " م ", eng: " pm" };
+
+    // Set to true to use a 12 hour date format
+    var format_12hour = format_ampm;
+
+    var d = new Date();
+    var mm, days;
+    switch (lang) {
+        case 'ar':
+            mm = monthNames[d.getMonth()].ar;
+            days = daysList[d.getDay()].ar;
+            break;
+        case 'eng':
+            mm = monthNames[d.getMonth()].eng;
+            days = daysList[d.getDay()].eng;
+            break;
+        default:
+            break;
+    }
+    var dd = d.getDate();
+    var min = (min = ('0' + d.getMinutes()).slice(-2));
+    var hh = d.getHours();
+    var ampm = '';
+
+    // var d = new Date();
+    // var mm = monthNames[d.getMonth()];
+    // var days = daysList[d.getDay()];
+    // var dd = d.getDate();
+    // var min = (mins = ('0' + d.getMinutes()).slice(-2));
+    // var hh = d.getHours();
+    // var ampm = '';
+
+    if (format_12hour) {
+        switch (lang) {
+            case 'ar':
+                ampm = hh >= 12 ? pm.ar : am.ar;
+                break;
+            case 'eng':
+                ampm = hh >= 12 ? pm.eng : am.eng;
+                break;
+            default:
+                break;
+        }
+        hh = hh % 12;
+        hh = hh ? hh : 12; //show mod 0 as 12
+    }
+
+    if (lang == 'ar') {
+        document.getElementById('sblock__date').setAttribute("dir","rtl");
+    }
+
+    document.getElementById('hour').innerText = hh;
+    document.getElementById('separator').innerHTML = ' : ';
+    document.getElementById('minutes').innerText = min + ampm;
+
+    document.getElementById('month').innerText = mm;
+    document.getElementById('day').innerText = dd;
+    document.getElementById('dayS').innerText = days + ",";
+
+    setTimeout(displayClock, 1000);
+}
